ff97ffdcbedcf0aac80fd7bfb7a23754f948d8af
FawksX <xfawks@gmail.com>
2021-01-12 14:22:46 +0000

    Update PSCoreInfo.java

--- src/main/java/games/synx/pscore/PSCoreInfo.java
+++ src/main/java/games/synx/pscore/PSCoreInfo.java
@@ -6,6 +6,6 @@ public class PSCoreInfo {
     public static final String NAME = "PSCore";
     public static final String AUTHORS = "FawksX";
     public static final String VERSION = "1.0.0";
-    public static final String DESCRIPTION = "Pixelmon Skyblock Core";
+    public static final String DESCRIPTION = "PSCore";
 
 }
