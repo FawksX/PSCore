285b6b9beae22477ab40e7a3d4694660e2c6df78
FawksX <xfawks@gmail.com>
2021-01-23 02:24:27 +0000

    beginnings of custom help cmd

--- src/main/java/games/synx/pscore/command/PSCommandManager.java
+++ src/main/java/games/synx/pscore/command/PSCommandManager.java
@@ -1,10 +1,14 @@
 package games.synx.pscore.command;
 
+import co.aikar.commands.MessageType;
 import co.aikar.commands.SpongeCommandManager;
 import games.synx.pscore.manager.AbstractManager;
 import games.synx.pscore.manager.IManager;
 import org.slf4j.Logger;
 import org.spongepowered.api.plugin.PluginContainer;
+import org.spongepowered.api.text.format.TextColors;
+
+import java.util.Locale;
 
 public abstract class PSCommandManager extends AbstractManager implements IManager {
 
@@ -19,6 +23,8 @@ public abstract class PSCommandManager extends AbstractManager implements IManag
         registerPlayerCommands();
         registerAdminCommands();
 
+        registerFormats();
+
     }
 
     public abstract void registerPlayerCommands();
@@ -26,6 +32,11 @@ public abstract class PSCommandManager extends AbstractManager implements IManag
 
     public SpongeCommandManager getCommandManager() {
         return this.spongeCommandManager;
+    }
+
+    public void registerFormats() {
+        getCommandManager().setFormat(MessageType.HELP, TextColors.GREEN, TextColors.GRAY, TextColors.DARK_GRAY, TextColors.AQUA, TextColors.GOLD);
+        getCommandManager().getLocales().addMessageBundle("acf-pscore_en.properties", Locale.ENGLISH);
     }
 
 }
